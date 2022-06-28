046b17428f12fba1a32d54919609de027f4c9cb0
Salathiel Genese <salathiel@genese.name>
2022-06-28 06:22:25 +0200

    feat: make link with descriptive text where it leads

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 //!
 //! A RESP parser implementation, written with edge performance in mind.
 //!
-//! If you are not familiar with RESP, consider starting here with its specs:
-//! https://redis.io/docs/reference/protocol-spec/
+//! If you are not familiar with RESP, consider starting here with
+//! [RESP specs](https://redis.io/docs/reference/protocol-spec/).
 //!
 //! Should you find some issues, please report on GitHub project, or consider opening a pull-request.
 
